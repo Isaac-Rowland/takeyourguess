210c7a50018b7db494be681991381dd0d6737f9d
Isaac <ibirowland@gmail.com>
2022-04-07 22:31:45 +1000

    fixed initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import GuessPage from './GuessPage';
+
+function App() {
+  // TODO: Move state variables here
+  // make api calls here
+
+  return (
+    <div className="App">
+      <GuessPage />
+    </div>
+  );
+}
+
+export default App;
